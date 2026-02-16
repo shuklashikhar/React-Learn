b3903d139abe2204cf42b081438a2d77b1cc9445
shuklashikhar <shuklashikhar2004@gmail.com>
2026-02-16 22:49:17 +0530

    Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,16 @@
-import Cleanup from "./effects/Cleanup"
-import Dependencies from "./effects/Dependencies"
-import EffectTiming from "./effects/EffectTiming"
-import Bubbling from "./event-forms/Bubbling"
-import Focus from "./event-forms/Focus"
-import LargeForm from "./event-forms/LargeForm"
-import Propagation from "./event-forms/Propagation"
-import { RefVsState } from "./event-forms/RefVsState"
-import SyntheticEvent from "./event-forms/SyntheticEvent"
-import Validation from "./event-forms/Validation"
-import ConditionalRender from "./jsx-components/ConditionalRender"
-import ExpressionsVsStatements from "./jsx-components/ExpressionsVsStatements"
-import Fragments from "./jsx-components/Fragments"
-import JsxIsJsx from "./jsx-components/Jsx"
-import ListKeys from "./jsx-components/ListKeys"
-import Layout from "./patterns/Layout"
-import { UserContainer } from "./patterns/UserContainer"
-import NoRerender from "./refs/NoRerender"
-import PersistRef from "./refs/PersistRef"
-import PreviousValue from "./refs/PreviousValue"
-import AsyncState from "./rendering/AsyncState"
-import BatchingDemo from "./rendering/Batching"
-import Parent from "./rendering/Parent"
-import Predict from "./rendering/Predict"
-import RenderCounter from "./rendering/RenderCounter"
-import SnapshotDemo from "./rendering/SnapShot"
-import DuplicateState from "./state/DuplicateState"
-import LazyInit from "./state/LazyInit"
-import LiftState from "./state/LiftState"
+import React from "react";
+
 function App() {
   return (
-     <UserContainer/>
-  )
+    <div className="grid md:grid-cols-3">
+      <div className="bg-red-500">A</div>
+      <div className="bg-yellow-500">B</div>
+      <div className="bg-green-500" >C</div>
+    </div>
+  );
 }
 
-export default App
+
+
+
+export default App;
